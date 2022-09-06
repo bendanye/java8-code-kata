a0dfb061ec3b60b04c5ad285fb39796e439289ae
Ng Shen Soon Benjamin <shen_soonng@hotmail.com>
2022-09-06 12:27:16 +0800

    Update ExtraExercise4Test.java

--- stream-api/src/test/java/stream/api/ExtraExercise4Test.java
+++ stream-api/src/test/java/stream/api/ExtraExercise4Test.java
@@ -1,5 +1,6 @@
 package stream.api;
 
+import common.test.tool.annotation.Easy;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -18,17 +19,26 @@ import static org.junit.Assert.assertThat;
 
 public class ExtraExercise4Test {
 
-    @Test
+    @Easy @Test
     public void shouldGetCorrectSpeech() {
         String result = speech(1, 2);
 
         assertThat(result, equalTo("Hello Java Stream\nPlease continue to practice!"));
     }
 
+    @Easy @Test
+    public void primeNumber() {
+        assertThat(isPrime(0), equalTo(false));
+        assertThat(isPrime(1), equalTo(false));
+        assertThat(isPrime(2), equalTo(true));
+        assertThat(isPrime(4), equalTo(false));
+        assertThat(isPrime(5), equalTo(true));
+    }
+
     private static String speech(int startSentence, int endSentence) {
         StringBuilder builder = new StringBuilder();
 
-        for(int i = startSentence; i<=endSentence; i++) {
+        for (int i = startSentence; i <= endSentence; i++) {
             builder.append(sentence(i)).append("\n");
         }
 
@@ -36,7 +46,22 @@ public class ExtraExercise4Test {
     }
 
     private static String sentence(int index) {
-        String[] phrases = new String[] {"Hello Java Stream", "Please continue to practice!"};
+        String[] phrases = new String[]{"Hello Java Stream", "Please continue to practice!"};
         return phrases[index - 1];
+    }
+
+    private boolean isPrime(int number) {
+        if (number == 0 || number == 1) {
+            return false;
+        }
+
+
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
