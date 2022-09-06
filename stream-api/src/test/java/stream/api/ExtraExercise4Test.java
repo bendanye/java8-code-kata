a02689a96a3dd89e710e3d8f452e60a7ca3a6e05
Ng Shen Soon Benjamin <shen_soonng@hotmail.com>
2022-09-06 10:30:24 +0800

    Add files via upload

---
+++ stream-api/src/test/java/stream/api/ExtraExercise4Test.java
@@ -0,0 +1,41 @@
+package stream.api;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.junit.Assert.assertThat;
+
+
+/**
+ * Collections from various exercises that can be refactored to use function programming.
+ */
+
+public class ExtraExercise4Test {
+
+    @Test
+    public void shouldGetCorrectSpeech() {
+        String result = speech(1, 2);
+
+        assertThat(result, equalTo("Hello Java Stream\nPlease continue to practice!"));
+    }
+
+    private static String speech(int startSentence, int endSentence) {
+        return IntStream
+                .rangeClosed(startSentence, endSentence)
+                .mapToObj(ExtraExercise4Test::sentence)
+                .collect(Collectors.joining("\n"));
+    }
+
+    private static String sentence(int index) {
+        String[] phrases = new String[] {"Hello Java Stream", "Please continue to practice!"};
+        return phrases[index - 1];
+    }
+}
