f8f413e9bfb718c7ed8f2b1c0b5c49b8e677e6d7
Ng Shen Soon Benjamin <shen_soonng@hotmail.com>
2022-09-06 09:09:24 +0800

    Add files via upload

---
+++ stream-api/src/test/java/stream/api/ExtraExercise2Test.java
@@ -0,0 +1,56 @@
+package stream.api;
+
+import common.test.tool.annotation.Easy;
+import common.test.tool.dataset.ClassicOnlineStore;
+import common.test.tool.entity.Customer;
+import common.test.tool.entity.Item;
+import common.test.tool.entity.Shop;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class ExtraExercise2Test extends ClassicOnlineStore {
+
+    @Easy @Test
+    public void getShopItemsTotalAmount() {
+        List<Shop> shopStream = this.mall.getShopList();
+
+        /**
+         * Use Java 8 Stream with reduce to get BigDecimal
+         */
+        BigDecimal sum = shopStream.stream()
+                .flatMap(shop -> shop.getItemList().stream())
+                .map(item -> new BigDecimal(item.getPrice()))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        assertThat(sum, is(new BigDecimal(60930)));
+    }
+
+    @Easy @Test
+    public void upperCaseNameOfAge30AndAbove() {
+        List<Customer> customers = this.mall.getCustomerList();
+
+        List<String> names = customers.stream()
+                .filter(customer -> customer.getAge() > 30)
+                .map(customer -> customer.getName().toUpperCase())
+                .limit(3)
+                .collect(Collectors.toList());
+
+        assertThat(names, hasItems("DIANA", "ALICE", "ANDREW"));
+    }
+    
+    @Test
+    public void groupByAge() {
+        List<Customer> customers = this.mall.getCustomerList();
+
+        Map<Integer, List<Customer>> results = customers.stream().collect(Collectors.groupingBy(Customer::getAge));
+
+        assertThat(results.keySet(), hasItems(32, 35, 36, 21, 38, 22, 26, 27, 28));
+    }
+}
