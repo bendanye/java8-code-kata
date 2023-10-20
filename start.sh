4902a63e9ef4a5e0e328a66b8b5de9041f03095d
benjamin.ng <benjamin.ng@thoughtworks.com>
2023-10-20 08:55:44 +0800

    Script to start and stop timer

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( dirname -- "$0"; )
+
+DATA_FILE="$SCRIPT_DIR/time_taken.txt"
+
+if ! test -f "$DATA_FILE"; then
+    echo "start_date,time_taken_in_secs,notes" > $DATA_FILE
+fi
+
+START=$(date +%s)
+
+echo "java8-code-kata Started. Please any keys to stop"
+read
+
+END=$(date +%s)
+secs=$((END-START))
+
+START_FORMATTED=$(date -r $START '+%Y-%m-%d %H:%M:%S')
+echo "$START_FORMATTED,$secs,NA" >> $DATA_FILE
+
+echo "java8-code-kata Completed!"
